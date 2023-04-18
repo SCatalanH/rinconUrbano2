5462abb53e6a49d428200934c86a1c504e16945d
SCatalanH <scatalanhermosilla@gmail.com>
2023-04-18 11:40:41 +0200

    V5 before Vienna/agregando articulos

--- core/models.py
+++ core/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 #Definicmo la tabla de Categorias primero 
+
+##### Tablas de Prueba #####
 class Categoria(models.Model):
     idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de Categoria')
     nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la Categoria')
@@ -18,4 +21,40 @@ class vehiculo(models.Model):
 
     def __str__(self):
         return self.patente
+    
+###### Tablas reales #######
+
+# django_website/main/models.py
+# django_website/main/models
+
+class ArticleSeries(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, default='', blank=True)
+    slug = models.SlugField("Series slug", null=False, blank=False, unique=True)
+    published = models.DateTimeField('Date published', default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = "Series"
+        ordering = ['-published']
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, default="", blank=True)
+    article_slug = models.SlugField("Article slug", null=False, blank=False, unique=True)
+    content = models.TextField()
+    published = models.DateTimeField("Date published", default=timezone.now)
+    modified = models.DateTimeField("Date modified", default=timezone.now)
+    series = models.ForeignKey(ArticleSeries, default="", verbose_name="Series", on_delete=models.SET_DEFAULT)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def slug(self):
+        return self.article_slug
+
+    class Meta:
+        verbose_name_plural = "Article"
+        ordering = ['-published']
+
     
